af90f480270dc5cecb7cc6e1ee9c66f7e38555cf
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-02-10 14:52:28 +0530

    Added navbar structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import './css/App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
-
-export default App
