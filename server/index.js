5f1f0a6a7b6c7373709d62f6c5d2a7beb61b30e1
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-02-11 19:40:15 +0530

    Render deployment

--- server/index.js
+++ server/index.js
@@ -4,17 +4,10 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, './client/dist')));
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/dist/index.html"));
-})
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
